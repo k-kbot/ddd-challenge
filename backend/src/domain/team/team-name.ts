0d6c63b93b8a083ba3b6d59df1fc63a430e0fa0e
k-kbot <polykkpodh1243@gmail.com>
2023-01-29 11:57:46 +0900

    名前の文字制限を修正

--- backend/src/domain/team/team-name.ts
+++ backend/src/domain/team/team-name.ts
@@ -1,11 +1,14 @@
 export class TeamName {
+  private static readonly MAX_LENGTH = 3;
   private static readonly REGEX = /\d/;
 
   private constructor(private value: string) {}
 
   static build(value: string): TeamName {
-    if (!this.REGEX.test(value)) {
-      throw new Error('チーム名は数字1文字のみが許可されています。');
+    if (value.length > this.MAX_LENGTH || !this.REGEX.test(value)) {
+      throw new Error(
+        `チーム名は${this.MAX_LENGTH}文字以下の数字のみが許可されています。`,
+      );
     }
 
     return new TeamName(value);
