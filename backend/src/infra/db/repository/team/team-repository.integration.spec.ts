5fa299111aada65e042a2112993f5e467596e10e
k-kbot <polykkpodh1243@gmail.com>
2023-02-05 14:00:33 +0900

    チーム名のユニーク制約を追加 (#13)

--- backend/src/infra/db/repository/team/team-repository.integration.spec.ts
+++ backend/src/infra/db/repository/team/team-repository.integration.spec.ts
@@ -100,6 +100,52 @@ describe('TeamRepository', () => {
     });
   });
 
+  describe('findByName', () => {
+    it('正常系 nameでチームを取得できること', async () => {
+      // Arrange
+      const name = '001';
+
+      await prisma.team.create({
+        data: {
+          id: '1',
+          name,
+          createdAt: '2023-01-01T09:00:00Z',
+          updatedAt: '2023-01-01T09:00:00Z',
+        },
+      });
+
+      // Act
+      const actual = await teamRepository.findByName(name);
+
+      // Assert
+      expect(actual).toStrictEqual(
+        new TeamDto({
+          id: '1',
+          name,
+          createdAt: new Date('2023-01-01T09:00:00Z'),
+          updatedAt: new Date('2023-01-01T09:00:00Z'),
+        }),
+      );
+    });
+
+    it('正常系 nameが存在しない場合はundefinedを返すこと', async () => {
+      // Arrange
+      await prisma.team.create({
+        data: {
+          id: '1',
+          name: '001',
+          createdAt: '2023-01-01T09:00:00Z',
+          updatedAt: '2023-01-01T09:00:00Z',
+        },
+      });
+
+      // Act
+      const actual = await teamRepository.findByName('002');
+
+      // Assert
+      expect(actual).toBeUndefined();
+    });
+  });
   describe('update', () => {
     it('正常系 チームの名前を更新できること', async () => {
       // Arrange
