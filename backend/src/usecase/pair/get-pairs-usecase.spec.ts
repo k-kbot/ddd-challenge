ce88a0fd4f37107df28c4f28fad7ef61041ea36d
k-kbot <polykkpodh1243@gmail.com>
2023-02-12 17:22:18 +0900

    ペア一覧取得のusecaseの作成

---
+++ backend/src/usecase/pair/get-pairs-usecase.spec.ts
@@ -0,0 +1,26 @@
+import { GetPairsUsecase } from './get-pairs-usecase';
+import { PairRepository } from '../../infra/db/repository/pair/pair-repository';
+import { PrismaClient } from '@prisma/client';
+
+jest.mock('@prisma/client');
+jest.mock('../../infra/db/repository/pair/pair-repository');
+
+describe('GetPairsUsecase', () => {
+  let mockRepository: jest.MockedObjectDeep<PairRepository>;
+
+  beforeAll(() => {
+    const prisma = new PrismaClient();
+    mockRepository = jest.mocked(new PairRepository(prisma));
+  });
+
+  describe('do', () => {
+    it('正常系 PairRepository.findAllを1回コールすること', async () => {
+      // Act
+      const usecase = new GetPairsUsecase(mockRepository);
+      await usecase.do();
+
+      // Assert
+      expect(mockRepository.findAll).toHaveBeenCalledTimes(1);
+    });
+  });
+});
