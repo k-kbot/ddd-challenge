bda13a52deb219905fb5c0ba6ae2fc66bfac352b
k-kbot <polykkpodh1243@gmail.com>
2023-02-18 16:22:52 +0900

    参加者一覧取得のusecaseの作成 (#26)

---
+++ backend/src/usecase/participant/get-participants-usecase.spec.ts
@@ -0,0 +1,26 @@
+import { GetParticipantsUsecase } from './get-participants-usecase';
+import { ParticipantRepository } from '../../infra/db/repository/participant/participant-repository';
+import { PrismaClient } from '@prisma/client';
+
+jest.mock('@prisma/client');
+jest.mock('../../infra/db/repository/participant/participant-repository');
+
+describe('GetParticipantsUsecase', () => {
+  let mockRepository: jest.MockedObjectDeep<ParticipantRepository>;
+
+  beforeAll(() => {
+    const prisma = new PrismaClient();
+    mockRepository = jest.mocked(new ParticipantRepository(prisma));
+  });
+
+  describe('do', () => {
+    it('正常系 ParticipantRepository.findAllを1回コールすること', async () => {
+      // Act
+      const usecase = new GetParticipantsUsecase(mockRepository);
+      await usecase.do();
+
+      // Assert
+      expect(mockRepository.findAll).toHaveBeenCalledTimes(1);
+    });
+  });
+});
